7b7b09ccb25886976966fcd3863ffff95e52f71f
mete0rfish <sungwon326@naver.com>
2025-10-20 08:23:08 +0900

    parser 추가

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Interpreter_Go
+
+## Parser Method Flow
+
+```mermaid
+sequenceDiagram
+    participant C as Client
+    participant P as Parser
+    participant L as Lexer
+
+    C->>P: New(lexer)
+    P->>L: NextToken()
+    P->>L: NextToken()
+    note right of P: curToken, peekToken 초기화
+    P-->>C: Parser 인스턴스
+
+    C->>P: ParserProgram()
+    loop until curToken is EOF
+        P->>P: parseStatement()
+        alt curToken is LET
+            P->>P: parseLetStatement()
+            P->>P: expectedPeek(IDENT)
+            P->>P: expectedPeek(ASSIGN)
+            note right of P: TODO: Expression 파싱 구현 필요
+            P-->>P: LetStatement
+        else
+            P-->>P: nil
+        end
+        P->>L: NextToken()
+    end
+    P-->>C: Program (AST 루트)
+
+```
+1. Lexer가 토큰화를 하여 Parser에게 데이터 스트림을 넘김
+2. Parser에선 해당 토큰이 적절한 순서로 이루어져있는 확인
+   1. *ast.Program이라는 AST 루트 노드를 만든다.
+   2. Let
---
+++ ast/ast.go
@@ -0,0 +1,144 @@
+package ast
+
+import (
+	"bytes"
+	"monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier // 변수 바인딩 식별자
+	Value Expression  // 값을 생성하는 표현식
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token // 'return' 토큰
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
+func (i *Identifier) String() string { return i.Value }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+type PrefixExpression struct {
+	Token    token.Token // 전위 연산자 토큰, 예를 들어!
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
